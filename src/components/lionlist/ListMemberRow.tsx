da488fb1af95e708dd93862ee8d82e618314c16e
Gyeongbin <tarabin49@gmail.com>
2023-09-23 11:07:49 +0900

    [Style]LionList 지원자 파트별 색상 styled-component 방식 props로 수정

--- src/components/lionlist/ListMemberRow.tsx
+++ src/components/lionlist/ListMemberRow.tsx
@@ -1,11 +1,11 @@
-import { styled } from "styled-components"
+import { css, styled } from "styled-components"
 import { GridContent } from "../../styles/globalStyle"
 import { memberType } from "./ListTable"
 
 const ListMemberRow = (props:memberType) => {
   return (
     <GridContent className="member">
-        <PartHighlight className={props.part}>{props.part}</PartHighlight>
+        <PartHighlight part={props.part}>{props.part}</PartHighlight>
         <div>{props.name}</div>
         <div>{props.primeNum}</div>
         <div>{props.firstScore}</div>
@@ -18,20 +18,27 @@ const ListMemberRow = (props:memberType) => {
 
 export default ListMemberRow
 
-const PartHighlight = styled.div`
+const PartHighlight = styled.div<{part:string}>`
     width: fit-content;
     padding: 0.2rem 0.7rem 0.2rem 0.7rem;
     border-radius: 10px;
-    &.기획{
-        background-color : #FAEDCC;
-    }
-    &.디자인{
-        background-color : #DEECDC;
-    }
-    &.프론트엔드{
-        background-color : #D6E4EE;
-    }
-    &.백엔드{
-        background-color : #E6DEED;
-    }
+    ${(props) => {
+        if(props.part === '기획'){
+            return css`
+                background-color: #FAEDCC;
+            `
+        } else if(props.part === '디자인'){
+            return css`
+                background-color: #DEECDC;
+            `
+        } else if(props.part === '프론트엔드'){
+            return css`
+                background-color: #D6E4EE;
+            `
+        } else {
+            return css`
+                background-color: #E6DEED;
+            `
+        }
+    }}
 `
