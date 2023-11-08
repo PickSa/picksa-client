dbbe12fe53125493d196bef36ae25e58932f7c14
kiiiiv <lizzy2@naver.com>
2023-11-08 20:32:33 +0900

    .

--- src/router.tsx
+++ src/router.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     },
     {
         path: "/pre-lionlist",
-        element: <Timetable />,
+        element: <LionList />,
     },
     {
         path: "/evaluate",
