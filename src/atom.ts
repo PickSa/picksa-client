d275cb33935487aa77472051747cd9d2ee30ebb9
Gyeongbin Bak <tarabin49@gmail.com>
2023-12-23 13:47:48 +0900

    [CHORE]테스트를 위한 atom default 값 수정

--- src/atom.ts
+++ src/atom.ts
@@ -6,9 +6,11 @@ const { persistAtom } = recoilPersist()
 export const UserInfoAtom = atom({
     key: "userInfo",
     default: {
-        isUser: false,
+        // isUser: false,
+        isUser: true, //test
         user: {
-            username: "",
+            // username: "",
+            username: "test", //test
         },
     },
     effects_UNSTABLE: [persistAtom],
@@ -16,7 +18,10 @@ export const UserInfoAtom = atom({
 
 export const accessTokenAtom = atom<string>({
     key: "access_token",
-    default: "",
+    // default: "",
+    default: "eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjIiLCJwb3NpdGlvbiI6IkdFTkVSQUwiLCJwYXJ0IjoiRlJPTlRFTkQiLCJpYXQiOjE3MDMzMDY3OTYsImV4cCI6MTcwMzM5MzE5Nn0.ANt1GJLvMAEJaFxQP-bqlW5DocHDfcZIaKyHo7175xChqY6c_52x32rvK1P69ROtmUjxgni8idyfyQj7KTudfw", 
+    //test
+    
     effects_UNSTABLE: [persistAtom],
 })
 
