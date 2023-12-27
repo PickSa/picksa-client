ac4edd06917091b89814090cbae7476c03299cfe
kiiiiv <lizzy2@naver.com>
2023-12-27 16:05:34 +0900

    [feat] 평가 api 거의 완료했으나 하나도 작동되지 않음

--- src/apis/Evaluate/PassFailComments.tsx
+++ src/apis/Evaluate/PassFailComments.tsx
@@ -2,19 +2,76 @@ import axios from "axios";
 export const baseUrl = 'https://picksa-server.o-r.kr';
 
 //리뷰 전체 정보 받기
-export const postEvaluation = async (applicant_id: string, 
-  managerId: string, passSelected:boolean, texts: string) => {
+export const postEvaluation = async (applicant_id: number, 
+  passSelected:boolean, texts: string, accessToken: string) => {
     const requestBody = {
       pass: passSelected,
       comment: texts,
     };
     try{
-        const response = await axios.post(`${baseUrl}/api/v1/evaluations/{applicant_id}`, 
+        const response = await axios.post(`${baseUrl}/api/v1/evaluations/${applicant_id}`, 
         requestBody, {
-          headers: {managerId},
+          headers: {
+            "Authorization" : `Bearer ${accessToken}`
+          },
         });
         return response.data;
     }catch{
-      return false;
+      console.log("false")
     }
+}
+export const patchEvaluation = async (evaluationId: string, 
+  passSelected:boolean, texts: string, memberId: string) => {
+    const requestBody = {
+      pass: passSelected,
+      comment: texts,
+    };
+    const config = {
+      headers:{
+        memberId: memberId,
+      }
+    };
+    try{
+        const response = await axios.post(`${baseUrl}/api/v1/evaluations/${evaluationId}`, 
+        requestBody, config);
+        return response.data;
+    }catch(error){
+      console.error(error);
+      return error
+    }
+}
+//지원자 id를 통해 한 지원자에 대한 다른 운영진의 평가를 조회가능. 
+export const getEvalOthers = async (applicant_id: number) => {
+  try {
+      const response = await axios.get(
+        `${baseUrl}/api/v1/evaluations/applicant/${applicant_id}`);
+      console.log(response.data);
+  } catch{
+    console.log("false")
+  }
+}
+//지원자 평가 현황 조회
+export const getPassFail = async (applicant_id: number) => {
+  try {
+      const response = await axios.get(
+        `${baseUrl}/api/v1/evaluations/final/${applicant_id}`);
+      console.log(response.data);
+  } catch{
+    console.log("false")
+  }
+}
+//파트장이 결정하는 합불 결과
+export const patchLeaderPassFail = async (applicant_id: number, accessToken: string,
+  result: string) => {
+  try {
+      const response = await axios.patch
+      (`${baseUrl}/api/v1/evaluations/final/${applicant_id}`,
+      {result},
+      {headers: {Authorization: `Bearer ${accessToken}`}});      
+      console.log(response.data);
+      return response.data;
+  } catch(error){
+    console.log(error);
+    return false;
+  }
 }
--- src/atom.ts
+++ src/atom.ts
@@ -16,7 +16,8 @@ export const UserInfoAtom = atom({
 
 export const accessTokenAtom = atom<string>({
     key: "access_token",
-    default: "",
+    default: 
+    "eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjIiLCJwb3NpdGlvbiI6IlBSRVNJREVOVCIsInBhcnQiOiJGUk9OVEVORCIsImlhdCI6MTcwMzU3NDc5NywiZXhwIjoxNzAzNjYxMTk3fQ.rH0tEoGLVtS2CcO067uvTAzStQWzVdD9mrl-EvOosYKxrRqnC1L95xl-VJp_mRLwu1mTMzE4kSlPBPmuOXMvog",
     effects_UNSTABLE: [persistAtom],
 })
 
