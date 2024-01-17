8a1000e62aa759e09bc973132896a519f2772934
kiiiiv <lizzy2@naver.com>
2024-01-17 21:47:59 +0900

    [Feat]타임테이블 스타일 수정

--- src/atom.ts
+++ src/atom.ts
@@ -6,19 +6,19 @@ const { persistAtom } = recoilPersist()
 export const UserInfoAtom = atom({
     key: "userInfo",
     default: {
-        isUser: false,
+        isUser: true,
         user: {
-            username: "",
-            userrole: "",
+            username: "윤예원",
+            userrole: "회장단",
         },
     },
     effects_UNSTABLE: [persistAtom],
 });
 
 export const accessTokenAtom = atom<string>({
-    key: "access_token",
-    default: "",
-    effects_UNSTABLE: [persistAtom],
+    key: "accesstoken",
+    default: "eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjQiLCJwb3NpdGlvbiI6IlBSRVNJREVOVCIsInBhcnQiOiJGUk9OVEVORCIsImlhdCI6MTcwNTQ4ODMyNywiZXhwIjoxNzA1NTc0NzI3fQ.wv9uOnrVHyemlty23vuGglcWK-CLdZoG5uu8YbwqB99rVapPdyW4rfSqw7_2JPdj9I_hV65eM3egoZO99kK8aw",
+    //effects_UNSTABLE: [persistAtom],
 })
 
 
