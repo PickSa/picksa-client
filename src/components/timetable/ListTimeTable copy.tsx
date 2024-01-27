ec3e5ad3d387375bbf40fda4ccc96ebcb68455d4
kiiiiv <lizzy2@naver.com>
2024-01-27 15:28:37 +0900

    [Feat]타임테이블 모두 수정

--- src/components/timetable/ListTimeTable copy.tsx
+++ src/components/timetable/ListTimeTable copy.tsx
@@ -54,91 +54,71 @@ const ListTable = () => {
         return `${d.getMonth()+1}/${d.getDate()}`
     }
   return (
-    <TableWrapper>       
-         <Table2>
-            <tbody>  
-                <ListTitle2><td style={{ color: 'red' }}>날짜</td></ListTitle2>
-                <ListTitle2>시간</ListTitle2>       
-                {
-                    applicantLists && applicantLists.map((data: memberType, idx:number)=>(
-                        <>
-                        <tr key={idx}>
-                            <Text><PartHighlight part={data.part}>{getPartName(data.part)}</PartHighlight></Text>
-                            <Text>{data.name}</Text>
-                        </tr>
-                        </>
-                    ))            
-                }    
-            </tbody>            
-        </Table2>          
-        <Scrollable> 
-            <Table>
-                <tbody>
-
-                    {timeSlots1.map((_, index) => {
-                        if (index === 0) {
-                            return <Td1 style={{ color: 'red' }}>{scheduleDay1 ? formatDate(scheduleDay1.date) : ""}</Td1>;
-                        }
-                        else {
-                            return <td style={{width: '2em'}}></td>;
-                        }
-                    })}
-                    {timeSlots2.map((_, index) => {
-                        if (index === 0) {
-                            return <Td1 style={{ color: 'red' }}>{scheduleDay2 ? formatDate(scheduleDay2.date) : ""}</Td1>;
-                        }
-                        else {
-                            return <td style={{width: '2em'}}></td>;
-                        }
-                    })}
-                    
-
-                <tr>
-                    {timeSlots1.map((time, index) => (
-                        <Td1 key={index}>{time}</Td1>
-                    ))}
-                    {timeSlots2.map((time, index) => (
-                        <Td1 key={index}>{time}</Td1>
-                    ))}
+    <TableWrapper>
+      <Scrollable>
+        <Table>
+          <tbody>
+            <tr style={{height: "4rem", background: "#F7F8FA"}}>
+              <td style={{ position: 'sticky', top: 0, zIndex: 8, left:0, background: "#F7F8FA"}}><Text1>날짜</Text1></td>
+              <td style={{width: '20rem', position: 'sticky', top: 0, left:60, zIndex: 7, background: "#F7F8FA"}}></td>
+              {timeSlots1.map((_, index) => {
+                if (index < 1) {
+                  return <Td1 style={{ position: 'sticky', top: 0,left: 5, zIndex: 6, background: "#F7F8FA" }}>{scheduleDay1 ? formatDate(scheduleDay1.date) : ""}</Td1>;
+                }
+                else {
+                  return <Td1 style={{position: 'sticky', top: 0, zIndex: 6, background: "#F7F8FA"}}></Td1>;
+                }
+              })}
+              {timeSlots2.map((_, index) => {
+                if (index < 1) {
+                  return <Td1 style={{ position: 'sticky', top: 0, zIndex: 6, background: "#F7F8FA" }}>{scheduleDay2 ? formatDate(scheduleDay2.date) : ""}</Td1>;
+                }
+                else {
+                  return <Td1 style={{position: 'sticky', top: 0, zIndex: 6, background: "#F7F8FA"}}></Td1>;
+                }
+              })}
+            </tr>
+            <tr style={{background: "#F7F8FA"}}>
+              <Td1 style = {{ position: 'sticky', top: 40, zIndex: 8, left:0, background: "#F7F8FA" }}><Text1>시간</Text1></Td1>
+              <td style={{width: '20rem', position: 'sticky', top: 40, left:60, zIndex: 7, background: "#F7F8FA"}}><Text></Text></td>
+              {timeSlots1.map((time, index) => (
+                  <Td1 key={index} style={{ position: 'sticky', top: 40, left: 5, zIndex: 5, background: "#F7F8FA" }}>{time}</Td1>
+                ))}
+                {timeSlots2.map((time, index) => (
+                  <Td1 key={index} style={ {position: 'sticky', top: 40, zIndex: 5, background: "#F7F8FA"}}>{time}</Td1>
+                ))}
+            </tr>
+            {
+              applicantLists && applicantLists.map((data: memberType, idx:number)=>(
+                <tr key={idx} style={{background: "#F7F8FA"}}>
+                  <td style={{ position: 'sticky', left: 0, zIndex: 2, background: "#F7F8FA", top: 5 }}><PartHighlight part={data.part}>{getPartName(data.part)}</PartHighlight></td>
+                  <td style={{ position: 'sticky', left: 61, zIndex: 2, background: "#F7F8FA", top: 5 }}><Text>{data.name}</Text></td>
+                  {timeSlots1.map((_, i) => {
+                    if (data.available) {
+                      const availabilityArray = data.available.split('');
+                      const isAvailable = availabilityArray[i];
+                      return <Td key={i} style={{ position: 'sticky', zIndex: 1, left: 5, top: 6 }}>{isAvailable === '1' ? <Check /> : <Nocheck />}</Td>
+                    }
+                    else {
+                      return <Td key={i} style={{ position: 'sticky', zIndex: 1, left: 5, top: 6 }}><Nocheck /></Td>
+                    }
+                  })}
+                  {timeSlots2.map((_, i) => {
+                    if (data.available) {
+                      const availabilityArray = data.available.split('');
+                      const isAvailable = availabilityArray[i];
+                      return <Td key={i} style={{ position: 'sticky', zIndex: 1, top: 6 }}>{isAvailable === '1' ? <Check /> : <Nocheck />}</Td>
+                    }
+                    else {
+                      return <Td key={i} style={{ position: 'sticky', zIndex: 1, top: 6 }}><Nocheck /></Td>
+                    }
+                  })}
                 </tr>
-                        {
-                            
-                            applicantLists && applicantLists.map((data: memberType, idx:number)=>(
-                                   
-                                <ListTitle3 key={idx}>  
-                                            
-                                {timeSlots1.map((_, i) => {
-                                    if (data.available) {
-                                        const availabilityArray = data.available.split('');
-                                        const isAvailable = availabilityArray[i];
-                                        return <Td key={i}>{isAvailable === '1' ? <Check /> : <Nocheck />}</Td>
-                                    } 
-                                    // 'available' 값이 없으면 <Nocheck />를 렌더링
-                                    else {
-                                        return <Td key={i}><Nocheck /></Td>
-                                    }
-                                })}
-                                   
-                                            
-                                {timeSlots2.map((_, i) => {
-                                    if (data.available) {
-                                        const availabilityArray = data.available.split('');
-                                        const isAvailable = availabilityArray[i];
-                                        return <Td key={i}>{isAvailable === '1' ? <Check /> : <Nocheck />}</Td>
-                                    } 
-                                    // 'available' 값이 없으면 <Nocheck />를 렌더링
-                                    else {
-                                        return <Td key={i}><Nocheck /></Td>
-                                    }
-                                })}  
-                                </ListTitle3>                               
-                                
-                            ))                                      
-                        }  
-                        
-                    </tbody>                    
-                </Table>  
-                </Scrollable>     
+              ))
+            }
+          </tbody>
+        </Table>
+      </Scrollable>
     </TableWrapper>
   )
 }
@@ -159,50 +139,21 @@ const TableWrapper = styled.div`
     display: flex;
     flex-direction: row;
     width: 98%;
-    padding-left: 10px;
-    padding-right: 10px;
+    padding-left: 15px;
+    padding-right: 15px;
     font-size: 1.2rem;
-    overflow-y: scroll;
-
-    
+    height: 48rem;   
 `;
-const GridContent2 = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    padding-left: 10px;
-    padding-right: 10px;
-    font-size: 1.9rem;
-
-`
 const Table = styled.table`
-width: 100%
-gap: 0.3rem;
-`;
-const Table2 = styled.table`
-width: 18rem;
-text-align: center;
-font-size: 1.4rem;
-`;
-const ListTitle2 = styled.tr`
-justify-content: center;
-padding-left: 3rem;
-font-size: 1.4rem;
-`;
-const ListTitle4 = styled.tr`
-gap: 5rem;
-`;
-const ListTitle3 = styled.tr`
-padding-top: 1rem;
-gap: 5rem;
+width: 100%;
 `;
 const Td = styled.td`
 padding-top: 2rem;
 font-size: 1.4rem;
 `;
 const Td1 = styled.td`
-padding-bottom: 0.5rem;
 font-size: 1.4rem;
+width: 5rem;
 `;
 const ListTitle = styled.tr`
 padding-bottom: 1rem;
@@ -224,16 +175,22 @@ const Container = styled.div`
 overflow-x: scroll; // 가로 스크롤 추가
 width: 100%;
 `;
-const Text = styled.td`
-width: 6rem;
+const Text = styled.div`
+width: 8rem;
 font-size: 1.4rem;
+background-color: "#F7F8FA";
+z-index:7;
+text-align: center;
 `;
-const Text1 = styled.td`
-width: 5rem;
-padding-bottom: 0.5rem;
+
+const Text1 = styled.div`
+text-align: center;
+font-size: 1.4rem;
+background-color: "#F7F8FA";
 `;
 const PartHighlight = styled.div<{part:string}>`
     font-size: 1.4rem;
+    text-align: center;
     width: 4.5rem;
     padding: 0.5rem 0.5rem 0.5rem 0.9rem;
     border-radius: 10px;
@@ -274,10 +231,8 @@ const Sticky = styled.td`
     position: sticky;
     left: 0;
 `;
-
 const Scrollable = styled.td`
     overflow-x: scroll;
-    
-    padding: 5px;
     font-size: 1.4rem;
+    padding-right: 18px;
 `;
