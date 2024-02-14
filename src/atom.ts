d16f4976583e1550dd0ffc44505e7b5bcceebf6d
Gyeongbin Bak <tarabin49@gmail.com>
2024-02-14 18:01:27 +0900

    [CHORE]atom 지우기

--- src/atom.ts
+++ src/atom.ts
@@ -6,9 +6,9 @@ const { persistAtom } = recoilPersist()
 export const UserInfoAtom = atom({
     key: "userInfo",
     default: {
-        isUser: true,
+        isUser: false,
         user: {
-            username: "박경빈",
+            username: "",
             userrole: "",
         },
     },
@@ -17,7 +17,7 @@ export const UserInfoAtom = atom({
 
 export const accessTokenAtom = atom<string>({
     key: "access_token",
-    default: "eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjE2IiwicG9zaXRpb24iOiJHRU5FUkFMIiwicGFydCI6IkZST05URU5EIiwiaWF0IjoxNzA3OTAwOTIzLCJleHAiOjE3MDc5ODczMjN9.UN8Ul98Z7Dlj4hc8XqsR5gE1fGR6zycPZ0b0RfinU_-NVBxHF5J6Lx362w2bBFMldS50wPdNYS1MI87LAbxdyQ",
+    default: "",
     effects_UNSTABLE: [persistAtom],
 })
 
